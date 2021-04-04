ac4e1f262207be217d33f3293579e2224f98539d
diego.taub <diego.taub@accenture.com>
2021-04-04 20:47:44 -0300

    Solución con diccionario

---
+++ Ejercicio11/Ejercicio11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, char> Caracteres = new Dictionary<char, char>
+            {
+                { 'á', 'a' }, { 'Á', 'A' },
+                { 'é', 'e' }, { 'É', 'E' },
+                { 'í', 'i' }, { 'Í', 'I' },
+                { 'ó', 'o' }, { 'Ó', 'O' },
+                { 'ú', 'u' }, { 'Ú', 'U' },
+            };
+
+            Console.Write("Ingrese texto: ");
+            string ingreso = Console.ReadLine();
+            foreach (KeyValuePair<char, char> caracter in Caracteres){
+                ingreso = ingreso.Replace(caracter.Key, caracter.Value);
+            }
+            Console.WriteLine(ingreso);
+            Console.ReadKey();
+        }
+    }
+}
